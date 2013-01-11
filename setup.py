3835413e905c97ebcbb75f8020b366ebfef287f5
Paul Winkler <slinkp@gmail.com>
2013-01-11 18:37:57 +0000

    Python 2.5 compatibility

---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys
+import os.path
+from setuptools import setup, find_packages
+
+readme = os.path.join(os.path.dirname(__file__), 'README')
+long_description = open(readme).read()
+
+if sys.version_info < (3,):
+    install_requires=["pyrepl>=0.8.2"]
+else:
+    install_requires=[]
+
+
+setup(
+    name='fancycompleter',
+    version='0.4',
+    author='Antonio Cuni',
+    author_email='anto.cuni@gmail.com',
+    py_modules=['fancycompleter'],
+    url='http://bitbucket.org/antocuni/fancycompleter',
+    license='BSD',
+    description='colorful TAB completion for Python prompt',
+    long_description=long_description,
+    keywords='rlcompleter prompt tab color completion',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Topic :: Utilities",
+        ],
+    install_requires=install_requires,
+)
---
+++ testing/test_fancycompleter.py
@@ -0,0 +1,41 @@
+from fancycompleter import DefaultConfig, Completer, Color
+
+class ConfigForTest(DefaultConfig):
+    use_colors = False
+
+def test_complete_attribute():
+    compl = Completer({'a': None}, ConfigForTest)
+    assert compl.attr_matches('a.') == ['a.__']
+    matches = compl.attr_matches('a.__')
+    assert 'a.__class__' not in matches
+    assert '__class__' in matches
+    assert compl.attr_matches('a.__class') == ['a.__class__']
+
+def test_complete_attribute_colored():
+    class ColorConfig(DefaultConfig):
+        use_colors = True
+        color_by_type = {type: '31'}
+        
+    compl = Completer({'a': 42}, ColorConfig)
+    matches = compl.attr_matches('a.__')
+    for match in matches:
+        if Color.set('31', '__class__') in match:
+            break
+    else:
+        assert False
+    assert ' ' in matches
+
+def test_complete_global():
+    compl = Completer({'foobar': 1, 'foobazzz': 2}, ConfigForTest)
+    assert compl.global_matches('foo') == ['fooba']
+    matches = compl.global_matches('fooba')
+    assert set(matches) == set(['foobar', 'foobazzz', ' '])
+    assert compl.global_matches('foobaz') == ['foobazzz']
+
+def test_complete_with_indexer():
+    compl = Completer({'lst': [None,2,3]}, ConfigForTest)
+    assert compl.attr_matches('lst[0].') == ['lst[0].__']
+    matches = compl.attr_matches('lst[0].__')
+    assert 'lst[0].__class__' not in matches
+    assert '__class__' in matches
+    assert compl.attr_matches('lst[0].__class') == ['lst[0].__class__']
